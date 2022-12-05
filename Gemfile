4041aab2a9ba2c015acbf13a69021da44d336151
Zachary T Jones <zt.jones88@gmail.com>
2022-12-05 02:51:28 -0500

    Try implementing enough codes to display IBM logo rom

--- Gemfile
+++ Gemfile
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 gem 'ruby-sdl2', '~> 0.3.5'
+
+gem 'debug', '~> 1.7.0'
+
 group :test do
   gem 'rspec', '~> 3.10.0'
 end
--- lib/chip8.rb
+++ lib/chip8.rb
@@ -8,19 +8,55 @@ class Chip8
   attr_reader :stack, :sound_timer, :delay_timer,
               :V0, :V1, :V2, :V3, :V4, :V5, :V6, :V7, :V8, :V9,
               :VA, :VB, :VC, :VD, :VE, :VF
-  attr_accessor :pc, :memory, :i
+  attr_accessor :pc, :memory, :i, :display
 
   def initialize
     @stack = []
-    @memory = Array.new(4096)
+    @memory = Array.new(4096, 0)
     @pc = 0
+    @display = Array.new(64) { Array.new(32, 0) }
+    @v = Array.new(0xf)
+
+    load_rom('../ibm-logo.ch8')
+    @pc = START_ADDR
+  end
+
+  def load_rom(path)
+    f = File.open(path)
+
+    pc = START_ADDR
+    until f.eof?
+      memory[pc] = f.readbyte
+      pc += 1
+    end
   end
 
   def fetch
-    instruction = memory[pc..pc+1]
-    self.pc += 2
+    instruction = memory[@pc..@pc+1]
+    @pc += 2
 
     instruction
+  end
+
+  def run
+    running = true
+    @pc = START_ADDR
+    debug = File.new('debug.txt', 'w')
+
+    Kernel.trap("INT") { running = false }
+
+    while running
+      instruction = fetch
+
+      debug.puts(format('%<pc>04x %<a>02x %<b>02x ', { pc: @pc, a: instruction[0], b: instruction[1]}))
+      decode(instruction)
+    end
+  end
+
+  def step
+    print @pc
+    instruction = fetch
+    decode(instruction)
   end
 
   def decode(instruction)
@@ -31,37 +67,87 @@ class Chip8
     nn = instruction[1]
     nnn = address(x, y, n)
 
+    # binding.break
+
     case op
     when 0x0
       case y
       when 0xE
-        'clear screen'
+        clear_screen
       end
     when 0x1
-      self.pc = nnn
+      jump(nnn)
     when 0x6
       set_register(x, nn)
     when 0x7
       add(x, nn)
     when 0xA
       seti(nnn)
+    when 0xD
+      draw(x, y, n)
     end
   end
 
   # TODO: Registers are 8-bit, so figure out what should happen if value is > 255
   # TODO: Seriously, just use an instance variable called V and make it an array of len 15
   def set_register(register, value)
-    instance_variable_set("@V#{register}".to_sym, value)
+    # puts "V#{register} := value"
+    # instance_variable_set("@V#{register}".to_sym, value)
+    @v[register] = value
+  end
+
+  def jump(nnn)
+    @pc = nnn
   end
 
   def get_register(register)
-    instance_variable_get("@V#{register}")
+    # instance_variable_get("@V#{register}")
+    @v[register]
   end
 
   def draw(vx, vy, n)
     x = get_register(vx) % 64
     y = get_register(vy) % 32
-    set_register()
+    set_register(0xf, 0)
+
+    (0..n).each do |row|
+      # 1. Get `row` byte, offset from I
+      # 2. loop through pixels
+      # # 1. If current pixel is on (1), and pixel at X,Y is on, turn off pixel and  VF=1
+      # #    If current pixel is on (1), and pixel at X,Y is off, turn on pixel
+      # # 2. If right-edge of screen is reached, stop drawing this row
+      # # 3. increment x
+      # 3. Increment Y
+      pixels = memory[i + row]
+      8.times do |bit|
+        break if x + bit > 63
+
+        if pixels[bit] == 1 && display[x][y] == 1
+          display[x][y] = 0
+          set_register(0xf, 1)
+        elsif pixels[bit] == 1 && display[x][y].zero?
+          display[x][y] = 1
+        end
+      end
+    end
+  end
+
+  def clear_screen
+    @display = Array.new(64) { Array.new(32, 0) }
+  end
+
+  def print_screen
+    64.times do |x|
+      32.times do |y|
+        if display[x][y] == 1
+          print "\u25A9"
+        else
+          print " "
+        end
+      end
+
+      print "\n"
+    end
   end
 
   private
